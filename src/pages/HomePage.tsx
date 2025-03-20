bc964389e0a5d052394b0c088ee00217bbcd11ef
JayKataria23 <66278443+JayKataria23@users.noreply.github.com>
2025-03-20 12:46:07 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,99 +9,46 @@ export default function HomePage() {
     <div className="container mx-auto mt-10 text-center">
       <img src={KMLogo} alt="KM Logo" className="mx-auto mb-8 w-36 h-auto" />
 
-      <div className="space-y-6">
-        <Button
-          className="w-64 text-lg h-12"
-          onClick={() => navigate("/order-form")}
-        >
+      <div className="grid grid-cols-2 gap-4 place-items-center">
+        <Button className="w-32 h-32 text-lg" onClick={() => navigate("/order-form")}>
           Order Form
         </Button>
-        <br />
-        <Button
-          className="w-64 text-lg h-12"
-          onClick={() => navigate("/fast-order-form")}
-        >
+        <Button className="w-32 h-32 text-lg" onClick={() => navigate("/fast-order-form")}>
           Fast Order Form
         </Button>
-        <br />
-        <Button
-          className="w-64 text-lg h-12"
-          onClick={() => navigate("/order-list")}
-        >
+        <Button className="w-32 h-32 text-lg" onClick={() => navigate("/order-list")}>
           Order List
         </Button>
-        <br />
-        <Button
-          className="w-64 text-lg h-12"
-          onClick={() => navigate("/order-file")}
-        >
+        <Button className="w-32 h-32 text-lg" onClick={() => navigate("/order-file")}>
           Design Wise Order
         </Button>
-        <br />
-        <Button
-          className="w-64 text-lg h-12"
-          onClick={() => navigate("/party-file")}
-        >
+        <Button className="w-32 h-32 text-lg" onClick={() => navigate("/party-file")}>
           Party Wise Order
         </Button>
-        <br />
-        <Button
-          className="w-64 text-lg h-12"
-          onClick={() => navigate("/part-order-file")}
-        >
+        <Button className="w-32 h-32 text-lg" onClick={() => navigate("/part-order-file")}>
           Part Orders
         </Button>
-        <br />
-        <Button
-          className="w-64 text-lg h-12"
-          onClick={() => navigate("/bhiwandi-list")}
-        >
+        <Button className="w-32 h-32 text-lg" onClick={() => navigate("/bhiwandi-list")}>
           Bhiwandi List
         </Button>
-        <br />
-        <Button
-          className="w-64 text-lg h-12"
-          onClick={() => navigate("/party-dispatch-list")}
-        >
+        <Button className="w-32 h-32 text-lg" onClick={() => navigate("/party-dispatch-list")}>
           Party Dispatch List
         </Button>
-        <br />
-        <Button
-          className="w-64 text-lg h-12"
-          onClick={() => navigate("/dispatch-list")}
-        >
+        <Button className="w-32 h-32 text-lg" onClick={() => navigate("/dispatch-list")}>
           Dispatch List
         </Button>
-        <br />
-        <Button
-          className="w-64 text-lg h-12"
-          onClick={() => navigate("/create-challan")}
-        >
+        <Button className="w-32 h-32 text-lg" onClick={() => navigate("/create-challan")}>
           Create Challan
         </Button>
-        <br />
-        <Button
-          className="w-64 text-lg h-12"
-          onClick={() => navigate("/challan-list")}
-        >
+        <Button className="w-32 h-32 text-lg" onClick={() => navigate("/challan-list")}>
           Challan List
         </Button>
-        <br />
-        <Button
-          className="w-64 text-lg h-12"
-          onClick={() => navigate("/party-profiles")}
-        >
+        <Button className="w-32 h-32 text-lg" onClick={() => navigate("/party-profiles")}>
           Party Profiles
         </Button>
-        <br />
-        <Button
-          className="w-64 text-lg h-12"
-          onClick={() => navigate("/broker-transport")}
-        >
+        <Button className="w-32 h-32 text-lg" onClick={() => navigate("/broker-transport")}>
           Master
         </Button>
-
-        <br />
       </div>
     </div>
   );
